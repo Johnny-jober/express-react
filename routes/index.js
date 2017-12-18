71398d147f3bc2a978033d5527d965b34afa8387
Johnny <johnny@jober.io>
2017-12-18 15:14:41 +0900

    typo

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.post('/' function(req, res, next) {
+router.post('/', function(req, res, next) {
   console.log('req.body from Iamport', req.body);
 });
 
