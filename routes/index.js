ebc71fe7100499a04ecd8d6fc5d9e2d656c51779
Johnny <johnny@jober.io>
2017-12-18 15:09:44 +0900

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.post('/' function(req, res, next) {
+  console.log('req.body from Iamport', req.body);
+});
+
+module.exports = router;
