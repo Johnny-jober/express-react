4e3167c5c99a6141435a84ff3ef6b0065c31c4e3
Johnny <johnny@jober.io>
2017-12-20 22:05:55 +0900

    useless line deleted

--- app.js
+++ app.js
@@ -23,8 +23,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/test', express.static(path.join(__dirname, 'public/client/build')));
-app.use('/users', users);
+app.use('/test', express.static(path.join(__dirname, './public/client/build')));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
